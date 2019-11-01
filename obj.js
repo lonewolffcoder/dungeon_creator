66b792368c2d059173b31862d0f3c7b28d0a600b
Steven Kay <stevendkay@gmail.com>
2019-11-01 22:13:43 +0000

    added top plane for walls

--- obj.js
+++ obj.js
@@ -30,14 +30,52 @@ class OBJFileWriter {
     render() {
         for (let row=0;row<this.world.height; row++) {
             for (let col=0;col<this.world.width; col++) {
-                if (this.world.getCellAt(row,col)===1) {
+                // floor tile
+                if (this.world.getCellAt(row,col)===1 ||
+                    this.world.getCellAt(row,col)===3 ||
+                    this.world.getCellAt(row,col)===4
+                ) {
                     this.addFloorTile(row,col);
-                }
+                };
+                // wall tile
+                if (this.world.getCellAt(row,col)===2) {
+                    this.addWallTile(row,col);
+                };
             }
         }
     }
 
+    addWallTile(row,col) {
+        // add wall tile
+        let minx = parseFloat(row);
+        let miny = parseFloat(col);
+        let maxx = parseFloat(row+1);
+        let maxy = parseFloat(col+1);
+        let v1 = this.getNextVertexNumber();
+        let v2 = v1+1;
+        let v3 = v1+2;
+        let v4 = v1+3;
+        let v5 = v1+4;
+        let v6 = v1+5;
+        let v7 = v1+6;
+        let v8 = v1+7;
+        this.addVertex(minx,miny,0);
+        this.addVertex(maxx,miny,0);
+        this.addVertex(maxx,maxy,0);
+        this.addVertex(minx,maxy,0);
+        this.addVertex(minx,miny,1);
+        this.addVertex(maxx,miny,1);
+        this.addVertex(maxx,maxy,1);
+        this.addVertex(minx,maxy,1);
+        let nextFace = this.getNextFaceNumber();
+        this.addFace(v1,v2,v3,v4);
+        nextFace = this.getNextFaceNumber();
+        this.addFace(v5,v6,v7,v8);
+        
+    }
+
     addFloorTile(row,col) {
+        // add floor tile
         let minx = parseFloat(row);
         let miny = parseFloat(col);
         let maxx = parseFloat(row+1);
